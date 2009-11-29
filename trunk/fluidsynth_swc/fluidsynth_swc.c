1f4441d257918e94242cca0501c038ab415539ff
yoann.huiban@gmail.com <yoann.huiban@gmail.com@623be0c4-dd13-11de-bf67-198b766644d4>
2009-11-29 21:59:44 +0000

    Added GPL statements

--- trunk/fluidsynth_swc/fluidsynth_swc.c
+++ trunk/fluidsynth_swc/fluidsynth_swc.c
@@ -1,6 +1,19 @@
-/*
- * gcc fluidsynth_swc.c -O3 -I$ALCHEMY_HOME/fluidsynth/include -Wall -swc -o fluidsynth_swc.swc `pkg-config fluidsynth --libs`
- * gcc fluidsynth_swc.c -O3 -Wall -static -I$ALCHEMY_HOME/fluidsynth/include -L$ALCHEMY_HOME/fluidsynth/lib -lfluidsynth -swc -o fluidsynth_swc.swc 
+/* Flash Midi Player - A midi player for the Adobe Flash Player
+ *
+ * Copyright (C) 2009 Yoann Huiban, yoann.huiban@gmail.com.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 #include <stdio.h>
 #include <stdlib.h>
